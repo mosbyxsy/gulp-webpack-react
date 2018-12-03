f1e8b93f96b94e6456673fa8a8b3d1e2aee6b431
Mosby192 <545610830@qq.com>
2018-12-03 23:23:33 +0800

    增加webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack = require('webpack');
 var cleanWebpackPlugin = require('clean-webpack-plugin');
 var ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 var purifyCss = require('purifycss-webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var glob = require('glob');
 var path = require('path');
 
@@ -15,6 +16,20 @@ module.exports = {
 	},
 	resolve: {
         extensions: ['.js', '.jsx','.json'],
+    },
+    devServer: {
+        contentBase:false,
+        host: '127.0.0.1',
+        port: '8888',
+        inline: true,//webpack官方推荐
+        watchOptions: {
+            aggregateTimeout: 1000,//浏览器延迟多少秒更新
+            poll: 1000//每秒检查一次变动
+        },
+        compress: false,
+        historyApiFallback: true,//找不到页面默认跳index.html
+        hot: true,//启动热更新，必须搭配new webpack.HotModuleReplacementPlugin()插件
+        open: true
     },
 	module: {
 		rules: [
@@ -91,6 +106,12 @@ module.exports = {
 			paths: glob.sync(
                 path.resolve(__dirname, './+(src/*.jsx | *.html)')          
             )
-		})
+		}),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+		    template: "./src/template.html",
+            title: '测试页面',
+            inject: true
+        }),
 	]
 }
